bcd3d8ae2ccdbcbe154aecdc95540a2681b2ea35
00019196 <wiut.id.00019196@gmail.com>
2024-03-11 20:10:44 +0500

    models updated

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 import cv2
-import utils
+import dlib
 
-# Open the camera
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(r".\shape_predictor_68_face_landmarks.dat")
+
+gender_model = cv2.dnn.readNet(r".\deploy_gender.prototxt", r".\gender_net.caffemodel")
+age_model = cv2.dnn.readNet(r".\deploy_age.prototxt", r".\age_net.caffemodel")
+
+gender_categories = ['Male', 'Female']
+age_categories = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Read a frame from the camera
     ret, frame = cap.read()
 
-    # Detect faces in the frame
-    faces, gray = utils.detect_faces(frame)
+    if not ret:
+        break
+
+    faces = detector(frame, 0)
 
     for face in faces:
         x1, y1, x2, y2 = face.left(), face.top(), face.right(), face.bottom()
+
+        face_roi = frame[y1:y2, x1:x2]
+        if face_roi.size == 0:
+            continue
+
+        blob = cv2.dnn.blobFromImage(face_roi, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), False, False)
+
+        gender_model.setInput(blob)
+        gender_preds = gender_model.forward()
+        gender = gender_categories[gender_preds[0].argmax()]
+
+        age_model.setInput(blob)
+        age_preds = age_model.forward()
+        age = age_categories[age_preds[0].argmax()]
+
+        cv2.putText(frame, f"Gender: {gender}, Age: {age}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
-        face_img = frame[y1:y2, x1:x2].copy()
-        gender, age = utils.get_gender_and_age(face_img)
-        cv2.putText(frame, f"Gender: {gender}, Age: {age}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    cv2.imshow('Gender and Age Detection', frame)
+    cv2.imshow("Gender and Age Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
